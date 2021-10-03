9f3b9a7fde78e3e38777d6bb68a5dbbca18d872d
Владимир Шальков <shalkov@surfstudio.ru>
2021-10-03 21:10:41 +0300

    Добавленые базовые примеры работы с Rx

---
+++ app/src/main/java/ru/shalkoff/rxjavaexample/Consts.kt
@@ -0,0 +1,10 @@
+package ru.shalkoff.rxjavaexample
+
+object Consts {
+
+    const val LOGGER_TAG = "RX_LOGGER"
+
+    const val ON_SUBSCRIBE = "onSubscribe"
+    const val ON_ERROR = "onError"
+    const val ON_COMPLETE = "onComplete"
+}
---
+++ app/src/main/java/ru/shalkoff/rxjavaexample/MainActivity.kt
@@ -0,0 +1,20 @@
+package ru.shalkoff.rxjavaexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    private val observableExample1 = ObservableExample1()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        observableExample1.runExample3()
+    }
+}
